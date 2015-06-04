99995677ab6b7a3363e05537b22575f6b3be41a9
Patrick <patrick@digital-detox.co.uk>
2015-06-04 09:42:16 +0100

    content structure reconfig

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-<?php include('modules/scenery.php'); ?>
 <?php include('modules/stage.php'); ?>
+<?php include('modules/scenery.php'); ?>
 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
