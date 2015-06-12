9b39e11263121625d369bed91194ddd76d19f1a7
Patrick <patrick@digital-detox.co.uk>
2015-06-12 21:43:20 +0100

    header / parallax lines

--- modules/header.php
+++ modules/header.php
@@ -1,9 +1,22 @@
-<header>
 
-    <div class="header__name">
-        <span class="header__name--first">Patrick</span>
-        <hr class="header__name--slash">
-        <span class="header__name--last">Bower</span>
+<div class="content">
+
+    <div class="logo__bg">
+        <div class="logo__icon">
+            <object type="image/svg+xml" data="assets/icons/pb.svg"></object>
+        </div>
     </div>
 
-</header>
+    <header>
+        <div class="header-name">
+            <span class="header-name__first">Patrick</span>
+            <hr class="header-name__slash">
+            <span class="header-name__last">Bower</span>
+        </div>
+        <div class="sub-head">
+            <span class="sub-head__a">Coder</span>
+            <span class="sub-head__b">Creative</span>
+        </div>
+    </header>
+
+</div>
---
+++ modules/parallel-lines.php
@@ -0,0 +1,14 @@
+<div class="parallel-lines__cont">
+    <div class="parallel-line__wrap parallel-line--west">
+        <hr class="parallel-line">
+    </div>
+    <div class="parallel-line__wrap parallel-line--south">
+        <hr class="parallel-line">
+    </div>
+    <div class="parallel-line__wrap parallel-line--north">
+        <hr class="parallel-line">
+    </div>
+    <div class="parallel-line__wrap parallel-line--east">
+        <hr class="parallel-line">
+    </div>
+</div>
