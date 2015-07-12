70ccdb79c6560e6e427d5a3b2c217794b7440244
Patrick <patrick@digital-detox.co.uk>
2015-07-12 09:57:20 +0100

    updates
    ReDone content sections,
    Adjusted fonts.
    Added strike to sub heads.
    Tidy up SCSS.

--- modules/content.php
+++ modules/content.php
@@ -1,39 +1,39 @@
 <div class="content">
-    <div class="content-triangle__header">
+    <div class="content-section--header">
         <?php include('modules/header.php'); ?>
     </div>
-    <div class="content-triangle">
+    <div class="content-section">
         <div class="content-triangle__shape content-triangle__shape--zero">
             <div class="content__area content__area--zero">
-                One One One One One One One One One One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, temporibus. Provident nulla numquam, facilis earum?
+                <?php include('modules/intro.php'); ?>
             </div>
         </div>
     </div>
-    <div class="content-triangle">
+    <div class="content-section">
         <div class="content-triangle__shape content-triangle__shape--one">
             <div class="content__area content__area--one">
-                One One One One One One One One One One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, temporibus. Provident nulla numquam, facilis earum?
+                <?php include('modules/code.php'); ?>
             </div>
         </div>
     </div>
-    <div class="content-triangle">
+    <div class="content-section">
         <div class="content-triangle__shape content-triangle__shape--two">
             <div class="content__area content__area--two">
-                Two Two Two Two Two Two Two Two Two Two Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam maiores molestias sunt laudantium expedita itaque quas, et asperiores aut. Possimus distinctio optio molestiae repellendus consequuntur!
+                <?php //include('modules/code.php'); ?>
             </div>
         </div>
     </div>
-    <div class="content-triangle">
+    <div class="content-section">
         <div class="content-triangle__shape content-triangle__shape--three">
             <div class="content__area content__area--three">
-                Three Three Three Three Three Three Three Three Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum vel quos voluptatibus numquam molestias soluta accusamus dicta non quae similique. Autem at quibusdam numquam fugiat ullam, impedit odio obcaecati praesentium nulla fugit possimus itaque. Nobis, praesentium nesciunt corrupti eligendi deleniti quae aliquam recusandae vero.
+                <?php //include('modules/code.php'); ?>
             </div>
         </div>
     </div>
-    <div class="content-triangle">
+    <div class="content-section">
         <div class="content-triangle__shape content-triangle__shape--four">
             <div class="content__area content__area--four">
-                Four Four Four Four Four Four Four Four Four Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae ipsam, ratione officiis culpa obcaecati, soluta.
+                <?php //include('modules/code.php'); ?>
             </div>
         </div>
     </div>
