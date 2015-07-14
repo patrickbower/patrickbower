d15c3f7d3f7c67bb5e3575ab1b5510deccb490d4
Patrick <patrick@digital-detox.co.uk>
2015-07-14 17:57:57 +0100

    header changes
    Tablet styles added.
    Autoprefixer added to Grunt.
    SCSS files broken out into better sections.

---
+++ grunt/autoprefixer.js
@@ -0,0 +1,10 @@
+module.exports = {
+        options: {
+            browsers: ['last 2 versions', 'ie 8', 'ie 9']
+        },
+
+        dist: {
+            src: 'assets/css/main.css',
+            dest: 'assets/css/main.css'
+        }
+};
