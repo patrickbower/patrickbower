ce479e0fd8827640b1128c94b9737b18689454ab
Patrick <patrick@digital-detox.co.uk>
2015-07-19 19:55:07 +0100

    basic iconography applied to sections

--- modules/creative.php
+++ modules/creative.php
@@ -1,11 +1,11 @@
-<div class="code">
+<div class="creative">
     <div class="media">
-        <div class="media__img code__icon">
-            <i class="icon-bracket"></i>
+        <div class="media__img creative__icon">
+            <i class="icon-pencil"></i>
         </div>
         <div class="media__text">
             <h2>
-                <span class="strike code--strike">Code</span>
+                <span class="strike creative--strike">Creative</span>
             </h2>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa inventore cumque alias nihil deleniti repellat blanditiis, fugit deserunt qui voluptates odio commodi accusantium quae distinctio facilis reprehenderit aspernatur nesciunt aperiam, possimus at. Harum aperiam, a sapiente optio assumenda labore perspiciatis officiis commodi dicta, nam, voluptatibus cupiditate ipsa esse animi.
         </div>
