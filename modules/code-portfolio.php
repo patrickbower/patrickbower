d47094ca594a46cd4045b5b0a8ce61416b378d67
Paddy <bower.patrick@gmail.com>
2015-10-01 09:59:50 +0100

    set up carousel js and css

--- modules/code-portfolio.php
+++ modules/code-portfolio.php
@@ -1,12 +1,38 @@
 <!-- screen-takeover -->
 <div class="screen-takeover__content">
-    <img
-        alt="vodafone.com homepage"
+    <div class="carrossel">
+        
+        <ul class="carrossel__rail">
+            <li class="carrossel__slide">
+                <img
+                    alt="vodafone.com homepage"
 
-        srcset=" ../assets/img/vodafone.com-lrg.jpg 1200w,
-                 ../assets/img/vodafone.com-med.jpg 768w,
-                 ../assets/img/vodafone.com-sml.jpg 480w"
-    />
+                    srcset=" ../assets/img/vodafone.com-lrg.jpg 1200w,
+                             ../assets/img/vodafone.com-med.jpg 768w,
+                             ../assets/img/vodafone.com-sml.jpg 480w"
+                />
+            </li>
+            <li class="carrossel__slide">
+                <img
+                    alt="vodafone.com homepage"
+
+                    srcset=" ../assets/img/vodafone.com-lrg.jpg 1200w,
+                             ../assets/img/vodafone.com-med.jpg 768w,
+                             ../assets/img/vodafone.com-sml.jpg 480w"
+                />
+            </li>
+            <li class="carrossel__slide">
+                <img
+                    alt="vodafone.com homepage"
+
+                    srcset=" ../assets/img/vodafone.com-lrg.jpg 1200w,
+                             ../assets/img/vodafone.com-med.jpg 768w,
+                             ../assets/img/vodafone.com-sml.jpg 480w"
+                />
+            </li>
+        </ul>
+
+    </div>
     <h3 class="screen-takeover__heading">code here</h3>
 </div>
 <!--
---
+++ src/js/carrossel.js
@@ -0,0 +1,38 @@
+/**
+* @name parallax scroll
+* @author pb
+*
+*
+*
+*/
+
+// (function ($, pb) {
+// 	'use strict';
+//
+// 	var selectors = {
+// 		carrossel : 'carrossel'
+// 	};
+//
+// 	function Carrossel(line)
+// 	{
+//
+// 	};
+//
+// 	function SetUp()
+// 	{
+//
+// 	};
+//
+// 	function Events()
+// 	{
+//
+// 	};
+//
+// 	var carrossel = $('.' + selectors.carrossel);
+// 	var parallaxLine = new ParallaxLine(carrossel);
+//
+// 	// pb.carrossel = {
+// 	// 	Carrossel : Carrossel
+// 	// };
+//
+// }(jQuery, window.pb = window.pb || {}));
