bb4b888bd0738be594776e4f3b1e091639e20092
Paddy <bower.patrick@gmail.com>
2015-10-30 22:15:07 +0000

    project takeover layout update

--- modules/development-projects.php
+++ modules/development-projects.php
@@ -1,5 +1,4 @@
 <!-- screen-takeover -->
-<div class="screen-takeover__background"></div>
 <div class="belt">
 
     <a href="#" class="header-logo">
@@ -11,18 +10,6 @@
             <div class="column width-100">
                 <div class="screen-takeover__title">
                     <h2 class="screen-takeover__title">Development Projects</h2>
-                    <!-- <ul class="project__index-list">
-                        <li>
-                            <a class="sub-head no-underline" href="#">
-                                Vodafone Global
-                            </a>
-                        </li>
-                        <li>
-                            <a class="sub-head no-underline" href="#">
-                                Personal Site
-                            </a>
-                        </li>
-                    </ul> -->
                 </div>
             </div>
         </div>
@@ -36,12 +23,20 @@
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore libero nobis saepe consequatur repellendus qui provident voluptates aspernatur ea illo ducimus facere perspiciatis, tenetur ab sapiente distinctio nam reprehenderit corporis. Deleniti minus corporis aliquam et earum atque ut, illum dolorum quo, sapiente debitis animi accusantium hic temporibus enim quod suscipit velit ipsa, voluptatibus quae sit maiores quisquam necessitatibus perspiciatis. Est, doloremque ad enim dolor architecto aperiam voluptatem neque deleniti, repellat provident laboriosam qui earum sint ab, odio necessitatibus maxime natus.
                     </p>
+                    <div class="button__wrap">
+                        <a href="http://www.vodafone.com" class="button screen-takeover--button" target="_blank">
+                            Visit vodafone.com
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="column width-60">
-                <img src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
-                <img src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
-                <img src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
+                <div class="project__images">
+                    <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
+                    <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
+                    <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
+                    <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
+                </div>
             </div>
         </div>
     </div>
--- modules/development.php
+++ modules/development.php
@@ -5,7 +5,7 @@
         </div>
         <div class="media__text">
             <h2>
-                <span class="strike development--strike">Development</span>
+                <span class="strike development--strike">Coder</span>
             </h2>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa inventore cumque alias nihil deleniti repellat blanditiis, fugit deserunt qui voluptates odio commodi accusantium quae distinctio facilis reprehenderit aspernatur nesciunt aperiam, possimus at. Harum aperiam, a sapiente optio assumenda labore perspiciatis officiis commodi dicta, nam, voluptatibus cupiditate ipsa esse animi.
