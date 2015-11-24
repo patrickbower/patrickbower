03500916dfe64625c5bf547c6261cffd73d34415
Paddy <bower.patrick@gmail.com>
2015-11-24 14:09:02 +0000

    changed folders to match objectives

--- modules/development-projects.php
+++ modules/development-projects.php
@@ -1,21 +1,15 @@
 <!-- screen-takeover -->
 <div class="belt">
 
-    <a href="#" class="header-logo">
-        <i class="header-logo__icon icon-pb"></i>
-    </a>
-
     <div class="screen-takeover__content">
         <div class="screen-takeover__title">
             <h2 class="screen-takeover__title">Development Projects</h2>
         </div>
         <div class="card">
-            <!-- <div class="card__head">
-
-            </div> -->
-            <div class="card__body">
-                <div class="grid">
-                    <div class="column width-40">
+            <div class="grid">
+                <div class="column width-40">
+                    <div class="card__back">
+                        <div class="logo"></div>
                         <h3>Vodafone Global</h3>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore libero nobis saepe consequatur repellendus qui provident voluptates aspernatur ea illo ducimus facere perspiciatis, tenetur ab sapiente distinctio nam reprehenderit corporis.
@@ -23,13 +17,15 @@
                         <p>
                             Deleniti minus corporis aliquam et earum atque ut, illum dolorum quo, sapiente debitis animi accusantium hic temporibus enim quod suscipit velit ipsa, voluptatibus quae sit maiores quisquam necessitatibus perspiciatis. Est, doloremque ad enim dolor architecto aperiam voluptatem neque deleniti, repellat provident laboriosam qui earum sint ab.
                         </p>
-                        <div class="button__wrap">
+                        <div class="button__wrap card__btn">
                             <a href="http://www.vodafone.com" class="button screen-takeover--button" target="_blank">
                                 www.vodafone.com
                             </a>
                         </div>
                     </div>
-                    <div class="column width-60">
+                </div>
+                <div class="column width-60">
+                    <div class="card__front">
                         <div class="project__images">
                             <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
                             <img class="project__image" src="../assets/img/vodafone.com-lrg.jpg" alt="vodafone global site homepage">
@@ -41,4 +37,5 @@
             </div>
         </div>
     </div>
+    <div class="backgound--triangle"></div>
 </div>
