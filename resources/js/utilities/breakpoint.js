f27692d954e084be9eeea3f21b89c795d091d040
Paddy <bower.patrick@gmail.com>
2016-06-08 20:59:54 +0100

    Added breakpoint pseudo element to body. Added breakpoint js util function. Added functions to model launch that will link directly to page or display in model according to layout.

---
+++ resources/js/utilities/breakpoint.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+* Get the breakpoint set on :before of the body by CSS.
+*
+* @return {string} Breakpoint value.
+*/
+export function breakpoint(){
+
+    let breakpoint = {};
+    breakpoint.value = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+
+    return breakpoint;
+};
