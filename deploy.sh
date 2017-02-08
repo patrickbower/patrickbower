9bb006ff173e053624659b5df20138982f843012
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-02-08 00:20:55 +0000

    Improved messaging for deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git commit -am "Save local changes"
+git commit -am "Commit changes"
 git checkout -B gh-pages
 git add -f build
-git commit -am "Rebuild website"
+git commit -am "Run build"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
 git checkout -
