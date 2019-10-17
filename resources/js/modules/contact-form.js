1f9984f084fedbbbd10b9982e3bf4ae7b7369a84
patrickbower <bower.patrick@gmail.com>
2019-10-17 16:19:37 +0100

    Update contact form
    Includes
    - Remove custom JS reCapture
    - Add FormSpree native reCapture
    - Update HTML semantics
    - Remove deprecated CSS and JS

--- resources/js/modules/contact-form.js
+++ resources/js/modules/contact-form.js
@@ -4,8 +4,6 @@ const defaults = {
   element: undefined,
   selectors: {
     contact_form: "js-contact--form",
-    submit_button: "js-contact--submit",
-    confirm_input: "js-contact--confirm",
     contact_section: "js-contact--section"
   },
   email: {
@@ -17,7 +15,6 @@ const defaults = {
 /**
  * Enhance the contact form.
  *
-
  * @module Contact Form
  * @class ContactForm
  */
@@ -37,14 +34,12 @@ export class ContactForm {
   }
 
   /**
-   * Initalise and setup at runtime.
+   * Initialise and setup at runtime.
    *
    * @function init
    */
   init() {
     this.setFormAction();
-    this.hiddenInput();
-    this.bindEvents();
   }
 
   /**
@@ -56,65 +51,5 @@ export class ContactForm {
     let form = this.element.querySelector("." + this.selectors.contact_form);
     const action = `https://formspree.io/${this.email.first}@${this.email.last}`;
     form.setAttribute("action", action);
-  }
-
-  /**
-   * Add event listners where requiried.
-   *
-   * @function bindEvents
-   */
-  bindEvents() {
-    let instance = this;
-
-    // form submit button
-    let submit_form_event = {
-      handleEvent: function(event) {
-        event.preventDefault();
-        instance.submitForm();
-      }
-    };
-
-    let submit_button = this.element.querySelector(
-      "." + this.selectors.submit_button
-    );
-    submit_button.addEventListener("click", submit_form_event);
-  }
-
-  /**
-   * Prevent tabbing into hidden input placed to encourage spam bots.
-   *
-   * @function hiddenInput
-   * @param contactForm - The module html.
-   */
-  hiddenInput() {
-    let confirm = this.element.querySelector(
-      "." + this.selectors.confirm_input
-    );
-    confirm.tabIndex = -1;
-  }
-
-  /**
-   * Get content via Ajax.
-   *
-   * @function submitForm
-   * @requires {function} ajax utility
-   */
-  submitForm() {
-    let form = this.element.querySelector("." + this.selectors.contact_form);
-    utility.postJson(form.action, new FormData(form), data => {
-      this.confirmSubmit();
-    });
-  }
-
-  /**
-   * Replace contact form with confermation message.
-   *
-   */
-  confirmSubmit() {
-    let contact_section = this.element.querySelector(
-      "." + this.selectors.contact_section
-    );
-    contact_section.innerHTML =
-      "<h2 class='h2'><span class='strike contact-section--strike'>Thanks</span></h2>";
   }
 }
