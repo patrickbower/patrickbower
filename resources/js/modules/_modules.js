268a54e1d503910e7e0887743fef25e22e4a2de6
Patrick Bower <bower.patrick@gmail.com>
2019-11-12 16:20:08 +0000

    Small screen navigation (#78)
    * Resolve JS error
    
    Remove deprecated module from list
    
    * Add mobile navigation
    
    Includes
    - new navigation
    - back and next feature for each detail section
    - hide footer
    - adjust styles for branding

--- resources/js/modules/_modules.js
+++ resources/js/modules/_modules.js
@@ -1,5 +1,4 @@
 import { ContactForm } from "./contact-form";
-import { Email } from "./email";
 import { BackButton } from "./back-button";
 import { InView } from "./in-view";
 import { Modals } from "./modals";
@@ -10,8 +9,7 @@ let modules = {
   ["back-button"]: BackButton,
   modals: Modals,
   modal: Modal,
-  contact: ContactForm,
-  email: Email
+  contact: ContactForm
 };
 
 export { modules };
